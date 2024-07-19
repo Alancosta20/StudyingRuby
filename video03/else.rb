fedebc6ffa7da1d158e998ee74d3d26caf2fb98b
Zuadinho <alancostacs@hotmail.com>
2024-07-19 10:06:07 -0300

    Else e Como printar dentro de uma pasta

---
+++ video03/else.rb
@@ -0,0 +1,14 @@
+day = 'Saturday'
+
+if day == 'Sunday'
+  lunch = 'special'
+elsif day == 'Holiday'
+  lunch = 'later'
+else
+  lunch = 'normal'
+end
+
+puts "lunch is #{lunch} today"
+
+#para printar dentro de uma pasta:
+#"ruby nomedapasta/arquivo.rb"
